056353589cb05202eefccf3c564e539e3973e4c8
Mackenly Jones <53151058+mackenly@users.noreply.github.com>
2024-05-10 19:46:09 -0400

    Adds docs

---
+++ docs/README.md
@@ -0,0 +1,14 @@
+# Kavita Import Tool Docs 
+
+Based on [Nextra](https://nextra.site).
+
+Not looking to contribute? [Read the latest docs here](https://kavitaimporttool.mackenly.com/).
+
+## Local Development
+
+First, run `pnpm i` to install the dependencies.
+
+Then, run `pnpm dev` to start the development server and visit localhost:3000.
+
+## License
+Nextra is licensed under the MIT License, however, any modifications and the content are licensed under this project's license, found [here](../LICENSE).
---
+++ docs/theme.config.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+	logo: <span>Kavita Import Tool</span>,
+	project: {
+		link: 'https://github.com/mackenly/kavita-import-tool',
+	},
+	docsRepositoryBase: 'https://github.com/mackenly/kavita-import-tool-docs',
+	footer: {
+		text: (
+			<p>
+				Kavita Import Tool is a project by{' '}
+				<a href="https://github.com/mackenly" target="_blank">
+					Mackenly Jones
+				</a>
+				<br />
+				Sponsor or donate to this project via{' '}
+				<a href="https://github.com/sponsors/mackenly/" target="_blank">
+					GitHub Sponsors @mackenly
+				</a>
+			</p>
+		),
+	},
+	// favicon
+	head: (
+		<>
+		<meta name="theme-color" content="#000000" />
+		<meta
+			name="description"
+			content="Kavita Import Tool is a tool to help you create folder structures to import into Kavita"
+		/>
+		<link rel="icon" href="https://fav.farm/📖" />
+		</>
+	),
+};
+
+export default config
